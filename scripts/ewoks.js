97ac03b51adfc9c22f4ac340739910ee8d78fcf9
Richard Lui <test@test.com>
2022-02-19 18:23:55 +0000

    update

--- scripts/ewoks.js
+++ scripts/ewoks.js
@@ -3,11 +3,11 @@ import { check, sleep } from "k6";
 
 export let options = {
   stages: [
-      // Ramp-up from 1 to 200 virtual users (VUs) in 20s
-      { duration: "10s", target: 200},
+      // Ramp-up from 1 to 100 virtual users (VUs) in 20s
+      { duration: "10s", target: 100},
 
-      // Stay at rest on 200 VUs for 20s
-      { duration: "10s", target: 200 },
+      // Stay at rest on 100 VUs for 20s
+      { duration: "10s", target: 100 },
 
       // Ramp-down to 0 VUs for 10s
       { duration: "10s", target: 0 }
@@ -15,16 +15,13 @@ export let options = {
 };
 
 export default function () {
-  //const response = http.get("https://swapi.dev/api/people", {headers: {Accepts: "application/json"}});
-  //const response = http.get("https://blazedemo.com");
-  http.get("http://myapp:5000/");
-  const response1 = http.get('http://test.k6.io/');
+  const response1 = http.get('http://myapp:5000/');
   check(response1, {
     'verify homepage text': (r) =>
-      r.body.includes('Collection of simple web-pages suitable for load testing'),
+      r.body.includes('Python is fun'),
   });
 
-  const response2 = http.get("http://myapp:5000/db");
-  check(response2, { "status is 200": (r) => r.status === 200 });
+  //const response2 = http.get("http://myapp:5000/db");
+  //check(response2, { "status is 200": (r) => r.status === 200 });
   sleep(.300);
 };
